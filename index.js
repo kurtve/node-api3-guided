c5566469bb2da7b39e98bba4c6978e7cc5384666
Kurt Van Etten <kurt@vanetten.us>
2019-12-09 19:25:51 -0500

    Initial commit

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const express = require("express")
-const hubRouter = require("./routers/hub")
-const welcomeRouter = require("./routers/welcome")
+const express = require('express')
+const hubRouter = require('./routers/hub')
+const welcomeRouter = require('./routers/welcome')
 
-const server = express()
+const server = express();
 
-server.use(express.json())
+server.use(express.json());
 // Bring all our subroutes into the main application
 // (Remember, subroutes can have more children routers)
-server.use("/", welcomeRouter)
-server.use("/api/hubs", hubRouter)
+server.use('/', welcomeRouter)
+server.use('/api/hubs', hubRouter)
 
 server.listen(4000, () => {
-  console.log("\n*** Server Running on http://localhost:4000 ***\n")
-})
+  console.log('\n*** Server Running on http://localhost:4000 ***\n')
+});
